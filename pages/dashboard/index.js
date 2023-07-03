37f7a854469bb29d5640b910c40973944f3ed31f
Maximilian Holzmann <max@holzmann.io>
2023-07-02 19:57:31 -0500

    begins fetching from the api for api testing

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,13 +1,30 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import Navbar from '../../app/components/Navbar'
+import Navbar from "../../app/components/Navbar";
+
+const RAWG_KEY = process.env.RAWG_KEY;
+
+const gameCall = async () => {
+  const url = "https://api.rawg.io/api/games?key=" + RAWG_KEY;
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+gameCall();
 
 export default function Dashboard() {
   return (
     <>
-    <Navbar></Navbar>
-    <div className='text-center'>
-     <h1>Dashboard</h1>
-    </div>
+      <Navbar></Navbar>
+      <div className='text-center'>
+        <h1>Dashboard</h1>
+      </div>
     </>
   );
 }
