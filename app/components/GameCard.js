4c18b8ca1e668c0255e6be5884e558a64dc29fd4
MaxHolzmann <max@holzmann.io>
2023-07-04 17:09:31 -0500

    adds actual data fetching and uses useState to render and grab results for hypothetical search (not yet implemented)

---
+++ app/components/GameCard.js
@@ -0,0 +1,10 @@
+
+export default function GameCard() {
+    return (
+    <>
+    <div className="text-orange-400">
+    This is an example game card
+    </div>
+    </>
+    )
+}
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,29 +1,43 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import React, { useEffect, useState } from "react"
 import Navbar from "../../app/components/Navbar";
+import GameCard from "../../app/components/GameCard";
 
-const RAWG_KEY = process.env.RAWG_KEY;
-
-const gameCall = async () => {
-  const url = "https://api.rawg.io/api/games?key=" + RAWG_KEY;
-  try {
-    const response = await fetch(url);
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-gameCall();
 
 export default function Dashboard() {
+
+  const RAWG_KEY = process.env.NEXT_PUBLIC_RAWG_KEY;
+
+  const [results, setResults] = useState([])
+
+  const gameCall = async () => {
+    console.log('Game Call!');
+    const url = 'https://api.rawg.io/api/games?key=' + RAWG_KEY + '&search="paper mario"';
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        setResults(data.results)
+        console.log(results)
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    gameCall();
+  }, [])
+
   return (
     <>
       <Navbar></Navbar>
       <div className='text-center'>
         <h1>Dashboard</h1>
+        <GameCard></GameCard>
+        {results.map((result) => (
+          <div>{result.name}</div>
+        ))}
       </div>
     </>
   );
