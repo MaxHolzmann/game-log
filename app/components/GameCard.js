e693677b1fa27c0c8e02abd07c599ae8a78344a9
Maximilian Holzmann <max@holzmann.io>
2023-07-05 10:44:52 -0500

    search functionality now working

--- app/components/GameCard.js
+++ app/components/GameCard.js
@@ -1,11 +1,14 @@
-// create screenshots slider!? 
-export default function GameCard({result}) {
-    return (
+// create screenshots slider!?
+export default function GameCard({ result }) {
+  return (
     <>
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-    <div className="text-orange-400">{result.name}</div>
-    <img className="w-full h-48 object-cover" src={result.background_image}/>
-    </div>
+      <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+        <div className='text-orange-400'>{result.name}</div>
+        <img
+          className='w-full h-48 object-cover'
+          src={result.background_image}
+        />
+      </div>
     </>
-    )
+  );
 }
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,39 +1,50 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import Navbar from "../../app/components/Navbar";
 import GameCard from "../../app/components/GameCard";
 
-
 export default function Dashboard() {
-
   const RAWG_KEY = process.env.NEXT_PUBLIC_RAWG_KEY;
 
-  const [results, setResults] = useState([])
+  const [results, setResults] = useState([]);
 
-  const [search, setSearch] = useState([])
+  const [search, setSearch] = useState([]);
 
   const gameCall = async () => {
-    console.log('Game Call!');
-    const url = 'https://api.rawg.io/api/games?key=' + RAWG_KEY + '&search="paper mario"';
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        setResults(data.results)
-        console.log(results)
+    if (search == null || search == undefined || search == "") {
+      console.log("Search is null, undefined, or empty. Not calling API.");
+    } else {
+      console.log("Game Call! Search is: " + search);
+      const url =
+        "https://api.rawg.io/api/games?key=" +
+        RAWG_KEY +
+        '&search="' +
+        search +
+        '"';
+      try {
+        const response = await fetch(url);
+        if (response.ok) {
+          const data = await response.json();
+          setResults(data.results);
+          console.log(results);
+        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
     }
   };
 
-  const updateSearch = () => {
-    console.log('Clicked')
-  }
+  const updateSearch = (e) => {
+    e.preventDefault();
+    setSearch(e.target.form.search.value);
+    console.log("search updated! search is now: " + search);
+  };
 
   useEffect(() => {
     gameCall();
-  }, [])
+  }, [search]);
+
+  //TODO : FIx search not editing on first enter..
 
   return (
     <>
@@ -42,14 +53,21 @@ export default function Dashboard() {
         <h1>Dashboard</h1>
 
         <h2>Search Games</h2>
-        <input placeholder="Search for a game" className="border"></input>
-        <button onClick={updateSearch()} type="submit">Search</button>
+        <form>
+          <input
+            name='search'
+            placeholder='Search for a game'
+            className='border'
+          ></input>
+          <button onClick={updateSearch} type='submit'>
+            Search
+          </button>
+        </form>
 
-
-        <div className="grid grid-cols-3 gap-4 content-center">
-        {results.map((result) => (
-          <GameCard result={result}></GameCard>
-        ))}
+        <div className='grid grid-cols-3 gap-4 content-center'>
+          {results.map((result) => (
+            <GameCard result={result}></GameCard>
+          ))}
         </div>
       </div>
     </>
