06f0e27c58d741f43787c79cff5c60001570ac9b
MaxHolzmann <max@holzmann.io>
2023-07-17 15:47:27 -0500

    begins new kanban process utilizing a much better npm library

--- pages/kanban.js
+++ pages/kanban.js
@@ -1,48 +1,46 @@
-import { KanbanComponent, ColumnsDirective, ColumnDirective } from "@syncfusion/ej2-react-kanban";
-import * as React from 'react';
-import '../app/globals.css';
-import Navbar from '../app/components/Navbar'
+import { useSession, signIn, signOut } from "next-auth/react";
+import React, { useEffect, useState } from "react";
+import Navbar from "../app/components/Navbar";
+import GameCard from "../app/components/GameCard";
+import Column from "../app/components/Column";
 
+export default function Kanban() {
 
-/* GAME KANBAN TODO
-Fetch users session on load
-Fetch users kanban data on load (their kanban data will be their games)
-Display it only after it has been fetched
-Post any time there is an edit to the kanban board.
+ const initialData = {
+    tasks: {
+        'task-1': { id: 'task-1', content: 'Take out the garbage' },
+        'task-2': { id: 'task-2', content: 'Watch my favorite show' },
+        'task-3': { id: 'task-3', content: 'Charge my phone' },
+        'task-4': { id: 'task-4', content: 'Cook dinner' },
+      },
+    columns: {
+        'column-1': {
+            id: 'column-1',
+            title: 'To Do',
+            tasksIds: ['task-1', 'task-2', 'task-3', 'task-4']
+        }
+    },
+    columnOrder: ['column-1']
+ }
 
-*/
+  const { data: session, status } = useSession();
 
-// not sure if this kanban component is going to be the one. 
-//it is overly complicated for what im attempting to achieve
-const kanbanObj = new Kanban ({
-    cardSettings: {
-        template: "#cardTemplate"
-    }
-})
+  if (status === "loading") {
+    return <p>Loading!</p>;
+  }
 
-function Kanban() {
-    
-    let data = [
-        { Id: 1, Status: 'Close', Summary: 'Analyze the new requirements gathered from the customer.', Type: 'Story', Priority: 'Low', Tags: 'Analyze,Customer', Estimate: 3.5, Assignee: 'Nancy Davloio', RankId: 2 },
-        { Id: 2, Status: 'InProgress', Summary: 'Fix the issues reported in the IE browser.', Type: 'Bug', Priority: 'Release Breaker', Tags: 'IE', Estimate: 2.5, Assignee: 'Janet Leverling', RankId: 1  },
-        { Id: 3, Status: 'Testing', Summary: 'Fix the issues reported by the customer.', Type: 'Bug', Priority: 'Low', Tags: 'Customer', Estimate: '3.5', Assignee: 'Steven walker', RankId: 1 },
-        { Id: 4, Status: 'Close', Summary: 'Arrange a web meeting with the customer to get the login page requirements.', Type: 'Others', Priority: 'Low', Tags: 'Meeting', Estimate: 2, Assignee: 'Michael Suyama', RankId: 1 },
-        { Id: 5, Status: 'Validate', Summary: 'Validate new requirements', Type: 'Improvement', Priority: 'Low', Tags: 'Validation', Estimate: 1.5, Assignee: 'Robert King', RankId: 1 }
-    ];
-    return (
-        <>
-            <Navbar></Navbar>
-            <div className="App">
-                <KanbanComponent id="kanban" keyField="Status" dataSource={data} cardSettings={{ contentField: "Summary", headerField: "Id" }}>
-                    <ColumnsDirective>
-                    <ColumnDirective headerText="To Do" keyField="Open"/>
-                    <ColumnDirective headerText="In Progress" keyField="InProgress"/>
-                    <ColumnDirective headerText="Testing" keyField="Testing"/>
-                    <ColumnDirective headerText="Done" keyField="Close"/>
-                    </ColumnsDirective>
-                </KanbanComponent>
-            </div>
-            </>
-          );
-};
-export default Kanban;
+  // retrieve all games that match user, display here in position order and categorized.
+  state = initialData;
+
+  return (
+    <>
+     <Navbar></Navbar>
+     {this.state.columnOrder.map((columnId) => {
+        const column = this.state.columns[columnId]
+        const tasks = column.tasksIds.map(taskId => this.state.tasks[taskId])
+
+        return <Column key={column.id} column={column} tasks={column}></Column>
+     })}
+    </>
+  );
+}
