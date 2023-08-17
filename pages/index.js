936efafe7b1a3bab0e8b2ca436253e95c74f9af0
MaxHolzmann <max@holzmann.io>
2023-08-17 15:11:50 -0500

    adds hero component and utilizies it on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Hero from '../app/components/Hero'
 
 export default function Home() {
   return (
     <>
-      <div className='grid grid-cols-2'>
-        <h1 className='text-center text-4xl mt-20'>Game Log</h1>
-      </div>
+      <Hero></Hero>
     </>
   );
 }
